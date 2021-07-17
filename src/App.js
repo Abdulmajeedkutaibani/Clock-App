9f01202e54ba7e8df41c15381bdcc4071e493e5b
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-07-18 00:15:06 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import './index.css'
-
+import './index.css';
 
 function App() {
-  return (
-    <div className="container">
-     <h1>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h1>
-     <h2>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h2>
-     <h3>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h3>
-     <h4>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h4>
-     <h5>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h5>
-     <h6>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h6>
-    </div>
-  );
+  return <div className='container'></div>;
 }
 
 export default App;
