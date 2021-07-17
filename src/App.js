162f3c0d712e4bd1b8038269fa561a1afc4e111b
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-07-17 19:13:59 +0300

    added basic css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import './index.css'
 
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+     <h1>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h1>
+     <h2>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h2>
+     <h3>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h3>
+     <h4>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h4>
+     <h5>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h5>
+     <h6>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+</h6>
     </div>
   );
 }
